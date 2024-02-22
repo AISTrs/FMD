f9d757464d1c2bbc54845a91acd908f89a98ccaf
udaya2308 <udaya14apr2000@gmail.com>
2024-02-21 20:15:31 -0500

    update home page; add expense table and usage bar chart

--- src/apps/static/assets/js/home.js
+++ src/apps/static/assets/js/home.js
@@ -4,12 +4,13 @@ async function fetchApiJsonData(url) {
             .then(response => response.json());
         return data;
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error(`There was a problem with the fetch operation: ${url}`, error);
     }
 }
 
 var fiscalData = fetchApiJsonData("/api/fiscal_data/");
 
+var committeeExpenseData;
 
 // function to move home layout
 document.addEventListener('DOMContentLoaded', function () {
@@ -22,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var startDate = document.getElementById('start-date-label');
     var endDate = document.getElementById('end-date-label');
+    var tableLabel = document.getElementById('committee-table-label');
 
     nav.addEventListener('mouseleave', function () {
         homeDiv.style.left = '90px';
@@ -36,7 +38,10 @@ document.addEventListener('DOMContentLoaded', function () {
         fiscalData = data;
         startDate.innerText = `Start Date: ${data[0].start_date}`;
         endDate.innerText = `Start Date: ${data[0].end_date}`;
-        populateTable(data[0].id);
+        committeeExpenseData = fetchApiJsonData(`/api/committee_expense_data/${data[0].id}/`)
+        tableLabel.innerText = `${data[0].semester} expenses budget`;
+        populateTable();
+        populateScatterPlot();
         data.forEach(option => {
             const newOption = document.createElement("option");
             newOption.value = option.id;
@@ -51,60 +56,96 @@ document.addEventListener('DOMContentLoaded', function () {
         startDate.innerText = `Start Date: ${fiscalData[fiscalDropdown.selectedIndex].start_date}`;
         endDate.innerText = `Start Date: ${fiscalData[fiscalDropdown.selectedIndex].end_date}`;
 
-        populateTable(fiscalDropdown.value);
+        committeeExpenseData = fetchApiJsonData(`/api/committee_expense_data/${fiscalDropdown.value}/`)
+        populateTable();
+        populateScatterPlot();
+        tableLabel.innerText = `${fiscalData[fiscalDropdown.selectedIndex].semester} expenses budget`;
+
 
     });
 
 });
 
+function populateScatterPlot() {
+
+    committeeExpenseData.then(data => {
+
+        var filteredData = data.filter(item => item.usage !== null);
+
+        var committees = filteredData.map(item => item.committee);
+        var usage = filteredData.map(item => parseFloat(item.usage));
+
+        var trace = {
+            x: committees,
+            y: usage,
+            mode: 'markers',
+            type: 'bar',
+            marker: {
+                size: 8,
+                color: 'green'
+            }
+        };
+
+        var layout = {
+            title: 'Committee vs Usage',
+            xaxis: { title: 'Committee' },
+            yaxis: { title: 'Usage %' },
+            hoverlabel: {
+                bgcolor: 'orange',
+                font: { color: 'white' }
+            },
+            hovermode: 'closest',
+            hoverinfo: 'text'
+        };
+
+        Plotly.newPlot('committee-scatter-plot', [trace], layout);
+
+
+    });
+
+
+}
+
 
 // function to populate committee table 
-function populateTable(fiscal_id) {
+function populateTable() {
 
     var container = document.getElementById("tableContainer");
 
-    // Create a table element
+    container.innerHTML = "";
+
     var table = document.createElement('table');
     table.classList.add('table', 'table-bordered');
 
-    // Create table header
     var headerRow = document.createElement('tr');
     headerRow.classList.add('committee-table-header');
 
 
-    const header = ["Committee", "Budget", "Expenses", "Income", "Net", "Usage"]
+    const header = ["Committee", "Budget", "Income", "Expenses", "Net", "Usage"]
     header.forEach(key => {
         var headerCell = document.createElement('th');
-        headerCell.classList.add('p-2', 'text-primary');
+        headerCell.classList.add('p-6', 'text-primary', 'p-lg-1');
         headerCell.textContent = key.charAt(0).toUpperCase() + key.slice(1); // Capitalize first letter
         headerRow.appendChild(headerCell);
     });
     table.appendChild(headerRow);
 
-    // Create table rows and cells
+    committeeExpenseData.then(data => {
 
-    fetchApiJsonData(`/api/committee_expense_data/${fiscal_id}/`)
-        .then(data => {
-
-            console.log(data);
-
-            data.forEach(function (item) {
-                var row = document.createElement('tr');
-                row.classList.add('committee-table-row');
-                for (var key in item) {
-                    var cell = document.createElement('td');
-                    cell.classList.add('p-2');
-                    cell.textContent = item[key];
-                    row.appendChild(cell);
-                }
-                table.appendChild(row);
-            });
-
+        data.forEach(function (item) {
+            var row = document.createElement('tr');
+            row.classList.add('committee-table-row');
+            for (var key in item) {
+                var cell = document.createElement('td');
+                cell.classList.add('p-2');
+                cell.textContent = item[key];
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
         });
 
+    });
 
-
-    // Append the table to the container
     container.appendChild(table);
 
 }
